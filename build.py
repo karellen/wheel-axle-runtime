29a740353be0c08e35ffece401a198a7614e6b2a
Arcadiy Ivanov <arcadiy@ivanov.biz>
2023-02-28 19:59:54 -0500

    Release 0.0.3

--- build.py
+++ build.py
@@ -30,7 +30,7 @@ use_plugin("copy_resources")
 use_plugin("filter_resources")
 
 name = "wheel-axle-runtime"
-version = "0.0.3.dev"
+version = "0.0.3"
 
 summary = "Axle Runtime is the runtime part of the Python Wheel enhancement library"
 authors = [Author("Karellen, Inc.", "supervisor@karellen.co")]
